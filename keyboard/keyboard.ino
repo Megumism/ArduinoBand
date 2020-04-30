994e6a3c2dfbc5f66b24efb81f18979e8b6089d8
Megumism <philomegumi@gmail.com>
2020-04-30 18:36:10 +0800

    keyboard capable

--- keyboard/keyboard.ino
+++ keyboard/keyboard.ino
@@ -14,18 +14,18 @@ bool tested = false;
 void setup()
 {
     Serial.begin(9600);
-    for (int i = 0; i < row; i++)
+    for (int i = 0; i < col; i++)//列设为输出高电平
+    {
+        pinMode(colPins[i], OUTPUT);
+        digitalWrite(colPins[i], HIGH);
+        Serial.print(colPins[i]);
+        Serial.println(" Setup finished");
+    }
+    for (int i = 0; i < row; i++)//行设为读取，初始化为高
     {
         pinMode(rowPins[i], INPUT);
         digitalWrite(rowPins[i], HIGH);
         Serial.print(rowPins[i]);
-        Serial.println(" Setup finished");
-    }
-    for (int i = 0; i < col; i++)
-    {
-        pinMode(colPins[i], INPUT);
-        digitalWrite(colPins[i], HIGH);
-        Serial.print(colPins[i]);
         Serial.println(" Setup finished");
     }
 }
@@ -47,19 +47,17 @@ char getKey()
         digitalWrite(colPins[i], LOW);
         for (int j = 0; j < row; j++)
         {
-            if (!tested)
-            {
-                Serial.print(digitalRead(rowPins[j]));
-                Serial.print(i);
-                Serial.println(j);
-            }
+            // if (!tested)
+            // {
+            //     Serial.print(digitalRead(rowPins[j]));
+            //     Serial.print(i);
+            //     Serial.println(j);
+            // }
             if (digitalRead(rowPins[j]) == LOW)
             {
                 delay(debounce);
-                // while (digitalRead(rowPins[j]) == LOW) //一个有风险的死循环
-                // {
-                // }
-                // input = key[col][row];
+                while (digitalRead(rowPins[j]) == LOW); //一个有风险的死循环
+                input = key[i][j];
             }
         }
         digitalWrite(colPins[i], HIGH);
